41599da1a18fd6257bb5c1b0abafb0fca0e6ca97
Rahien <karel.kremer@gmail.com>
2023-05-01 18:27:42 +0200

    nest perk relations in character detail

--- packages/outpostapi/characters/serializers.py
+++ packages/outpostapi/characters/serializers.py
@@ -1,19 +1,5 @@
 from rest_framework import serializers
 from .models import Character, Perk, CharacterPerk
-class CharacterSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Character
-        fields = ["id","name", "class_name", "created_at", "user"]
-        read_only_fields = ["id", "created_at"]
-
-
-class CharacterDetailSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Character
-        fields = "__all__"
-        read_only_fields = ["id", "created_at"]
 
 class PerkSerializer(serializers.ModelSerializer):
 
@@ -25,7 +11,27 @@ class PerkSerializer(serializers.ModelSerializer):
 class CharacterPerkSerializer(serializers.ModelSerializer):
     perk = PerkSerializer(read_only=True, many=False)
 
+    def to_representation(self, instance):
+        char_perk = super().to_representation(instance)
+        return {**char_perk["perk"], "id": char_perk["id"], "active": char_perk["active"], "perk_id": char_perk["perk"]["id"]}
+
     class Meta:
         model = CharacterPerk
-        fields = "__all__"
+        fields = ["perk", "character_id", "id", "active"]
         read_only_fields = ["id"]
+
+class CharacterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Character
+        fields = ["id","name", "class_name", "created_at", "user"]
+        read_only_fields = ["id", "created_at"]
+
+
+class CharacterDetailSerializer(serializers.ModelSerializer):
+    perks = CharacterPerkSerializer(read_only=True, many=True, source="characterperk_set")
+
+    class Meta:
+        model = Character
+        fields = "__all__"
+        read_only_fields = ["id", "created_at", "perks"]
