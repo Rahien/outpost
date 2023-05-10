7188444d54337c5f8c613bbdacdfd4244f2063ab
Rahien <karel.kremer@gmail.com>
2023-05-10 18:29:38 +0200

    manifest and icons

--- packages/outpost-client/vite.config.ts
+++ packages/outpost-client/vite.config.ts
@@ -1,9 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxImportSource: "@emotion/react" })],
+  plugins: [
+    react({ jsxImportSource: "@emotion/react" }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "logo.svg",
+        "logo-192.png",
+        "logo-512.png",
+        "./src/assets/**/*",
+      ],
+      manifest: {
+        name: "Outpost",
+        short_name: "Outpost",
+        description: "A character management tool for *haven games",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "logo-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   define: {
     global: {},
   },
